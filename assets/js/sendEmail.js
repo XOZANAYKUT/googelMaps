fc68b0f193a3763c0ffa004fea79bd8d05fde5dd
XOZANAYKUT <>
2024-01-15 13:47:06 +0000

    update all file

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,15 +1,16 @@
 function sendMail(contactForm) {
-    emailjs.send("rosie", "template_q9do1eg", {
-        "name_from": contactForm.name.value,
-        "from_email": contactForm.email.value,
-        "project_request": contactForm.projectsummary.value,
+    emailjs.send("service_y0tv5oe", "service_y0tv5oe", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
     })
         .then(
             function (response) {
                 console.log("SUCCESS", response);
             },
             function (error) {
-                console.log("FAILED,error");
-            });
-    return false;
+                console.log("FAILED", error);
+            }
+        );
+    return false;  // To block from loading a new page
 }
